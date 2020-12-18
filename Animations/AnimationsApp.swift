02d3438c1f2c6035eac5d64099016b858b81becf
Alex Jang <alexjangspammyspam@gmail.com>
2020-12-17 22:17:44 -0800

    Initial Commit

---
+++ Animations/AnimationsApp.swift
@@ -0,0 +1,17 @@
+//
+//  AnimationsApp.swift
+//  Animations
+//
+//  Created by Alex Jang on 12/17/20.
+//
+
+import SwiftUI
+
+@main
+struct AnimationsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
